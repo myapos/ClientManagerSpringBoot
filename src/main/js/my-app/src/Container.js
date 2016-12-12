cc03e95cff817298d613b8f659f43ef728286e10
myapos <myapos@yahoo.com>
2016-12-12 23:19:48 +0200

    add react router and dashboard

---
+++ src/main/js/my-app/src/Container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from './actions/';
+import './App.css';
+
+class Container extends Component {
+  render() {
+    return (
+     <div> {this.props.children}</div>
+    )
+  }
+}
+
+
+export default connect(state => state, actions)(Container);
+
+
