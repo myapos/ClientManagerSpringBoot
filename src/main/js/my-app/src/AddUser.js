5b3f884dc029deffa703bee1cec96dc952265887
myapos <myapos@yahoo.com>
2016-12-18 15:15:18 +0200

    layout for add user screen

--- src/main/js/my-app/src/AddUser.js
+++ src/main/js/my-app/src/AddUser.js
@@ -33,117 +33,63 @@ class AddUser extends Component {
         <h2>Καλωσήρθατε στην διαχείριση πελατών του Ferrum Gym </h2>
         <div className="labelContainer">
           <legend><span>Προσθήκη πελάτη - Συμπληρώστε τα πεδία εγγραφής</span> <Link class="back" to="/"> Πίσω </Link></legend>
+        </div> 
           {/*Start of add user form*/}
-          <div className="container">
-            <form role="form" id="my_form" className="col-xs-12">
-                    <div className="form-group">
-                      <div className="row">
-                          <div className="col-xs-2"><label>Όνομα: <input className="form-control" type="text" name="fname" id="fname"/> </label></div>
-                          <div className="col-xs-2"><label>Επώνυμο:<input className="form-control" type="text" name="lname" id="lname"/> </label></div>
-                          <div className="col-xs-2"><label>Email: <input className="form-control" type="email" name="email" id="email"/> </label></div>
-                          <div className="col-xs-2"><label>Κινητό: <input className="form-control" type="number" name="mobile" id="mobile"/></label></div>
-                          <div className="col-xs-2">
-                          <label>Φύλλο: 
-                              <select className="form-control selectwidthauto" name="gender" id="gender">
-                                  <option value="Άρρεν">Άρρεν</option>
-                                  <option value="Θήλυ">Θήλυ</option>
-                              </select>
-                          </label>
-                          </div>
-                          <div className="col-xs-2"><label>Ηλικία: <input className="form-control" type="number" name="age" id="age"/></label></div>
-                      </div>
-                      <div className="row">   
-                          <div className="col-xs-4"><label>Ημερομηνία εγγραφής: <input className="form-control" type="date" name="date" id="date"/></label></div>
-                      </div>
-                    </div>    
-                    <div className="form-group">
-                    <fieldset>
-                      <legend>Τμήματα</legend>
-                      <div className="row"> 
-                        <div className="col-xs-2">
-                          <label>Kettlebells:
-                              <select className="form-control selectwidthauto" name="Kettlebells" id="Kettlebells">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                          </label>
-                        </div>
-                        <div className="col-xs-2">
-                        <label>Made In Brazil:
-                              <select className="form-control selectwidthauto" name="Made_In_Brazil" id="Made_In_Brazil">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label>
-                        </div>
-                        <div className="col-xs-2">
-                        <label>Krav Maga Stay Away:
-                              <select className="form-control selectwidthauto" name="Krav_Maga_Stay_Away" id="Krav_Maga_Stay_Away">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label>
-                        </div>
-                        <div className="col-xs-2">
-                        <label>Krav Maga Civilian:
-                              <select className="form-control selectwidthauto" name="Krav_Maga_Civilian" id="Krav_Maga_Civilian">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label>
-                        </div>
-                        <div className="col-xs-2">
-                        <label>Krav Maga Kiddy:
-                              <select className="form-control selectwidthauto" name="Krav_Maga_Kiddy" id="Krav_Maga_Kiddy">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label>
-                        </div>
-                        <div className="col-xs-2"><label>Krav Maga Kids:
-                              <select className="form-control selectwidthauto" name="Krav_Maga_Kids" id="Krav_Maga_Kids">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label>
-                        </div>
-                      </div>
-                      </fieldset>
-                    </div>  
-                    <div className="form-group"><legend>Επικοινωνία</legend>
-                      <div className="row">
-                        <div className="col-xs-2"><label>Λήψη sms?:
-                               <select className="form-control selectwidthauto" name="sms" id="sms">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label></div>
-                        <div className="col-xs-2"><label>Λήψη email?:
-                              <select className="form-control selectwidthauto" name="receive_email" id="receive_email">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                        </label></div>
-                      </div>
-                   </div>  
-                   <div className="form-group"><legend>Πληρωμές</legend>
-                      <div className="row">
-                        <div className="col-xs-2">
-                          <label>Πληρωμή?:
-                              <select className="form-control" name="payed" id="payed">
-                                  <option value="Ναι">Ναι</option>
-                                  <option value="Όχι">Όχι</option>
-                              </select>
-                          </label>
-                        </div>
-                        <div className="col-xs-4"><label>Ημερομηνία πληρωμής:<input className="form-control" type="date" name="payment_date" id="payment_date"/></label></div>
-                      </div>
-                     </div>
-                   <input type="submit" className="btn btn-default" value="Εγγραφή"/>
-               </form>
-          </div>
+         {/* <div className="container">*/}
+
+        <form method="post">
+            <div className="form-group">
+                <label for="name">Όνομα:</label>
+                <input className="form-control" type="text" id="firstname" name="user_name" />
+            </div>
+            <div className="form-group">
+                <label for="lastname">Επώνυμο:</label>
+                <input className="form-control" type="text" id="lastname" name="user_lastname" />
+            </div>
+            <div className="form-group">
+                <label for="mail">E-mail:</label>
+                <input className="form-control" type="email" id="mail" name="user_mail" />
+            </div>
+            <div className="form-group">
+                <label for="mobile">Κινητό:</label>
+                <input className="form-control" type="number" id="mobile" name="user_mobile" />
+            </div>
+            <div className="form-group">
+                <label for="gender">Φύλλο:</label>
+                <select className="form-control" name="gender" id="gender">
+                    <option value="Άρρεν">Άρρεν</option>
+                    <option value="Θήλυ">Θήλυ</option>
+                </select>
+            </div>            
+            <div className="form-group">
+                <label for="age">Ηλικία:</label>
+                <input className="form-control" type="number" id="age" name="user_age" />
+            </div>
+            <div className="form-group">
+                <label for="registrationDate">Ημερομηνία Εγγραφής:</label>
+                <input className="form-control" type="date" id="registrationDate" name="user_registration_date" />
+            </div>
+             <legend><span>Tμήματα Εγγραφής</span></legend>
+             <legend><span>Στοιχεία πληρωμής</span></legend>
+              <div className="form-group">
+                <label for="payment">Πληρωμή:</label>
+                <select className="form-control" name="payment" id="user_payment">
+                    <option value="Ναι">Ναι</option>
+                    <option value="Οχι">Οχι</option>
+                </select>
+              </div> 
+            <div className="form-group">
+                <label for="paymentDate">Ημερομηνία πληρωμής:</label>
+                <input className="form-control" type="date" id="paymentDate" name="user_payment_date" />
+            </div>
+            <div class="button">
+                <button className="btn btn-default" type="submit">Εγγραφή</button>
+            </div>
+        </form>
+
+          {/*</div>*/}
           {/*End of add user form*/}
-        </div>
+        
       </div>
 
     );
