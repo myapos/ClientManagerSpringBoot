97276df1341eeb445db1b317006ed048aa67d598
myapos <myapos@yahoo.com>
2017-01-14 15:29:31 +0200

    prepreload page when a new class is saved checked response status

--- src/main/js/my-app/src/api.js
+++ src/main/js/my-app/src/api.js
@@ -81,11 +81,6 @@ export const getPayeds = () => fetch(
 // Create the XHR object.
 function createCORSRequest1(method, url, data){
     let xhr1 = new XMLHttpRequest();
-    debugger;
- //    body={
-	// 	 		"description": "KVMG",
-	// 	        "studentClass": data		
-	// };
     if ("withCredentials" in xhr1){
         xhr1.open(method, url);
         xhr1.setRequestHeader("Authorization", 'Basic '+btoa('myapos:Apostolakis1981'));
@@ -103,75 +98,65 @@ function createCORSRequest1(method, url, data){
 };
 
 export const saveNewClass = (row) => {
-		 var request1 = createCORSRequest1("post", "http://localhost:8181/api/studentClasses");
+		 var request1 = createCORSRequest1("post", BASE_URL+"/api/studentClasses");
 
 		if (request1){
 		    request1.onload = function(){
+		    	//debugger;
 		        //do something with request.responseText
-		        console.log("sssssss:",request1.responseText);
-		        //parent.students = JSON.parse(request.responseText);
-		        //debugger;
+		        if(request1.status == 201){
+		        	alert("A new record has been created in database. Page is reloading");
+		        	console.log("responseText:",request1.responseText);
+		        	window.location.reload(true);
+		        }
 
 		    };
 
-		    request1.open("post", "http://localhost:8181/api/studentClasses");
+		    request1.open("post", BASE_URL+"/api/studentClasses");
 		    request1.setRequestHeader("Authorization", 'Basic '+btoa('myapos:Apostolakis1981'));
 		    request1.setRequestHeader("Content-type","application/json");
         	request1.contentType = "application/json";
-        	debugger;
-        	//let obj = JSON.parse(request2.responseText);
-        	//obj.id=1;
+
+        	/*this has to be fixed* sets subclass*/
         	let body= JSON.stringify({
 		 		"description": row.description,
 		        "studentClass": "http://localhost:8181/api/studentClasses/1"		
 			});
 		    request1.send(body);
 		}
- //   .then (res => res.json())
- //   .then(res => { 
-
-
- //   	debugger;
-	// 	console.log("-----api--------studentClasses from server: ",res);
-	// 	console.log("saving row",row.row);
-	// 	//const studentClasses = res._embedded.studentClasses;
-	// 	//return studentClasses;
-	// });
-
 }
+
+/*deletes selected class from table -- classId parameter is the id in front end table not in the database*/
+
 export const deleteStudentClass = (classId) => {
+
 		debugger;
 
 		console.log("hey from api.deleteStudentClass. Preparing to delete class with id:",classId);
 		let rowByClassId = document.querySelectorAll('tr')[classId];
 		let description = rowByClassId.childNodes[2].innerHTML;
-
+		//let requestUrl = 
 		//find class from database by description
 		//class description has to be unique
 		//example
 		//curl -v -u myapos:Apostolakis1981 http://localhost:8181/api/studentClasses/search/findBydescription?description=kettlebell
 
-
-
 		//  var request1 = createCORSRequest1("post", "http://localhost:8181/api/studentClasses");
-
 		// if (request1){
 		//     request1.onload = function(){
 		//         //do something with request.responseText
 		//         console.log("sssssss:",request1.responseText);
 		//         //parent.students = JSON.parse(request.responseText);
 		//         //debugger;
-
 		//     };
-
 		//     request1.open("post", "http://localhost:8181/api/studentClasses");
 		//     request1.setRequestHeader("Authorization", 'Basic '+btoa('myapos:Apostolakis1981'));
 		//     request1.setRequestHeader("Content-type","application/json");
-  //       	request1.contentType = "application/json";
-  //       	debugger;
-  //       	//let obj = JSON.parse(request2.responseText);
-  //       	//obj.id=1;
-  //       	let body= JSON.stringify({
+		//       	request1.contentType = "application/json";
+		//       	debugger;
+		//       	//let obj = JSON.parse(request2.responseText);
+		//       	//obj.id=1;
+		//       	let body= JSON.stringify({
 		//  		"description": row.row.description,
 		//         "studentClass": "http://localhost:8181/api/studentClasses/1"		
 		// 	});
