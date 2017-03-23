f1440108cdfd3c08cccbc1cac12e93a6d545de46
myapos <myapos@yahoo.com>
2017-03-23 02:54:37 +0200

    managed to set up webpack config dev

--- runthis.sh
+++ runthis.sh
@@ -2,10 +2,10 @@
 
 ORIGINAL_WD=$(pwd)
 PATH=$PATH:"$ORIGINAL_WD/target/node/":"$ORIGINAL_WD/target/node/node_modules/npm/bin/"
-JS_WD="$ORIGINAL_WD/src/main/js/my-app"
-echo "switching to js directory $JS_WD"
+JS_WD="$ORIGINAL_WD"
+echo "building client from $JS_WD"
 cd $JS_WD
 npm run build
-echo "switching to original directory $ORIGINAL_WD"
+echo "running server $ORIGINAL_WD"
 cd $ORIGINAL_WD
 $ORIGINAL_WD/mvnw spring-boot:run
