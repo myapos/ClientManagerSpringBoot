38f2c070ee0d4a878155aa56a457b6b968dd277b
myapos <myapos@yahoo.com>
2017-06-07 00:58:28 +0300

    changed myCommit file

--- myCommit.sh
+++ myCommit.sh
@@ -1,9 +1,9 @@
-echo "preparing to push changes"&&
-echo "Please give your commit message"&&
-read msg&&
-echo "Got commit message: $msg!"&&
-git status&&
-git add -A&&
-git commit -m "$msg"&&
-git push&&
+echo "preparing to push changes"
+echo "Please give your commit message"
+read msg
+echo "Got commit message: $msg!"
+git status
+git add -A
+git commit -m "$msg"
+git push
 echo "done!!"
