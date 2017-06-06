8a0c4659ac804ac948c0ed04db0ba6ab811f6276
myapos <myapos@yahoo.com>
2017-06-07 00:54:09 +0300

    test message

--- myCommit.sh
+++ myCommit.sh
@@ -1,6 +1,9 @@
 echo "preparing to push changes"&&
+echo "Please give your commit message"&&
+read msg&&
+echo "Got commit message: $msg!"&&
 git status&&
 git add -A&&
-git commit -m $1&&
+git commit -m "$msg"&&
 git push&&
-echo "done!"
+echo "done!!"
