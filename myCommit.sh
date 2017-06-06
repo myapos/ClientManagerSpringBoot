421d80d4f6e7a9e8352a1beb0fd85e20369b6b09
myapos <myapos@yahoo.com>
2017-06-07 01:04:36 +0300

    upload myCommit.sh to github

---
+++ myCommit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Preparing to push changes"
+echo "Please give your commit message"
+read msg
+echo "Got commit message: $msg!"
+git status
+git add -A
+git commit -m "$msg"
+git push
+echo "done!!"
