bed023a6121dde084b6fdd7736640c82ba64033b
myapos <myapos@yahoo.com>
2017-07-18 23:44:14 +0300

    improved preprocessRegistrations using ES6 functions

--- src/main/js/my-app/src/utils/preprocessRegistrations.js
+++ src/main/js/my-app/src/utils/preprocessRegistrations.js
@@ -1,30 +1,9 @@
 export default registrations => {
-  const sumAr = [];
-  let registrations_ = [];
-  registrations.map(curAr => {
-    // console.log('curAr:', curAr, 'l:', curAr.length);
-    for (let i = 0; i < curAr.length; i++) {
-      if (typeof curAr[i] !== 'undefined') {
-        // console.log('item:', curAr[i], 'l:', curAr[i].length);
-        if (curAr[i].length > 0) {
-          sumAr.push(curAr[i]);
-        }
-      }
-    }
-    return 1;
-  });
-
-  // debugger;
-  const reg_ = [];
-  sumAr.forEach((item, index) => {
-    console.log('item:', item);
-    item.map(i => {
-      reg_.push(i);
-      return i;
-    });
-  // debugger;
-
-  console.log('log:', reg_);
-  });
-  return reg_;
+  const sumAr = registrations.map(curAr => curAr.find(item => typeof item !== 'undefined'));
+  const s__ = sumAr.map((item, index1) =>
+    item.map((item2, index2) => {
+      item2.index = (index1 + index2 + 1);
+      return item2;
+    }));
+  return s__.reduce((sum, value) => sum.concat(value));
 };
